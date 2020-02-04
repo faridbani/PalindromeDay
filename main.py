405a0bf9488ce8ff5b44a28d0f41b78447ce5be0
faridbani <farid.banihashemi@gmail.com>
2020-02-04 22:05:01 +0100

    Function diff_years implemented

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# PalindromeDay sense 0000 00 00
+""" Palindrome days sense 0000 00 00 """
 import datetime
 
 global data
 global old_date
 
-date = datetime.datetime(1000, 1, 1)
-old_date = datetime.datetime(1000, 1, 1)
-
-print(date.strftime("%Y"))
+date = datetime.datetime(1000, 2, 1)
+old_date = datetime.datetime(1100, 1, 1)
 
 
 def is_palindrome(d):
@@ -19,6 +17,9 @@ def is_palindrome(d):
 
 def diff_years(date1, date2):
     """ Calculate the diff between the years"""
+    y1 = int(date1.strftime("%Y"))
+    y2 = int(date2.strftime("%Y"))
+    print(y2-y1)
 
 
 def during(old_da, d):
@@ -26,7 +27,8 @@ def during(old_da, d):
     pass
 
 
-def go_throu():
+def go_thr():
     pass
 
 
+diff_years(date,old_date)
