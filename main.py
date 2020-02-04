2b40a30919ab6cbb4ad54d1afe0ce10990a16abd
faridbani <farid.banihashemi@gmail.com>
2020-02-04 22:42:33 +0100

    Function is_palindrome implemented and tested

--- main.py
+++ main.py
@@ -4,22 +4,34 @@ import datetime
 global data
 global old_date
 
-date = datetime.datetime(1000, 2, 1)
+# initializing the global variables
+date = datetime.datetime(1020, 2, 1)
 old_date = datetime.datetime(1100, 1, 1)
 
 
 def is_palindrome(d):
     """ Checks if the date is a Palindrome day """
-    f = False
 
+    f = False
+    year = d.strftime("%Y")
+    mount = d.strftime("%m")
+    day = d.strftime("%d")
+    print(year, mount, day)
+
+    if (year[0] == day[1]) and \
+        (year[1] == day[0]) and \
+        (year[2] == mount[1]) and \
+        (year[3] == mount[0]):
+        f = True
     return f
 
 
 def diff_years(date1, date2):
     """ Calculate the diff between the years"""
+
     y1 = int(date1.strftime("%Y"))
     y2 = int(date2.strftime("%Y"))
-    print(y2-y1)
+    print(y2-y1, "years is gone.")
 
 
 def during(old_da, d):
@@ -31,4 +43,5 @@ def go_thr():
     pass
 
 
-diff_years(date,old_date)
+test = is_palindrome(date)
+print(test)
