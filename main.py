94e59c1948beb8a829eeb8678f5afd84d6b657ed
farid <farid.banihashemi@gmail.com>
2020-02-04 20:10:47 +0100

    Add the main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# PalindromeDay sense 0000 00 00
+
+
+def is_palindrome(date):
+    """ Checks if the date is a Palindrome day """
+    f = False
+    return f
+
+
+def diff_years(date1, date2):
+    """ Calculate the diff between the years"""
+    pass
+
+
+def during(old_date, date):
+    """ go throughout all days """
+    pass
+
+
+def go_throu():
+    pass
