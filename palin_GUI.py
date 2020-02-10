9bfa7f981ff792c0f43656f5d0629a76e47a4914
faridbani <farid.banihashemi@gmail.com>
2020-02-10 21:46:04 +0100

    The initial version of GUI

--- palin_GUI.py
+++ palin_GUI.py
@@ -1,2 +1,25 @@
-import logic
+#import logic
 
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QTextEdit, QHBoxLayout, QVBoxLayout
+
+
+class P_GUI:
+    def __init__(self):
+        #log = logic.Logic()
+        app = QApplication([])
+        app.setStyle("Fusion")
+        w = QWidget()
+        lab = QLabel("Please giv the begin and end year")
+        lay = QHBoxLayout()
+        y_b = QTextEdit("2020")
+        y_e = QTextEdit("2020")
+        lay.addWidget(lab)
+        lay.addWidget(y_b)
+        lay.addWidget(y_e)
+        w.setLayout(lay)
+        w.show()
+        app.setActiveWindow(w)
+        app.exec_()
+
+
+gui = P_GUI()
