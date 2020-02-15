b6c498cf90d9f848d4aa92e977fbc53e31da45e2
faridbani <farid.banihashemi@gmail.com>
2020-02-15 23:23:08 +0100

    Chande some comments in logic

--- logic.py
+++ logic.py
@@ -1,24 +1,25 @@
-""" Palindrome days sense 'date' / set it manual """
+""" All Palindrome days between tow dates """
 import datetime
-from logging import setLogRecordFactory
 
 
+# Help function to calculate how many years have gone between tow palindrome days
 def diff_years(date1, date2):
     """ Calculate the diff between the years"""
 
     # Convert the years data in to a digit
     y1 = int(date1.strftime("%Y"))
     y2 = int(date2.strftime("%Y"))
-    # print the difference
+    # Put the difference to String s and return it
     s = "After " + str(y2-y1) + " years:"
     return s
 
 
+# Help function to check if a date is a palindrome day
 def is_palindrome(d):
     """ Checks if the date is a Palindrome day """
 
     f = False
-    # Distract the date (year - mount - day)
+    # Distract the date (year - mount - day from d (datetime)
     year = d.strftime("%Y")
     mount = d.strftime("%m")
     day = d.strftime("%d")
@@ -33,9 +34,9 @@ def is_palindrome(d):
 
 class Logic:
     def __init__(self, d, m_d):
-        # initializing the global variables
+        # initializing the instance variables
         self.date = datetime.datetime(d, 1, 1)
-        self.max_date = datetime.datetime(m_d, 12, 30)
+        self.max_date = datetime.datetime(m_d, 12, 31)
         self.result = []
 
     def run(self):
@@ -47,7 +48,7 @@ class Logic:
         # set td to one day timedelta
         td = datetime.timedelta(1)
 
-        # Go through all days to find a Palindrome day and print it
+        # Go through all days to find a Palindrome day, put it in result[] and return result[]
         while date < self.max_date:
             if is_palindrome(date):
                 dif = diff_years(old_date, date)
@@ -59,7 +60,4 @@ class Logic:
             date += td
         return self.result
 
-
-""" my_run = Logic(2020, 2090)
-my_run.run() """
 
